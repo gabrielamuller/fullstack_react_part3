5f74092400a1a199d8d75f614e28c38d1de22ff2
gabrielamuller <mullergabrielaa@gmail.com>
2022-10-24 14:17:41 +0300

    3.19 Add validation so that name has to be at least three characters long

--- index.js
+++ index.js
@@ -57,21 +57,19 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 // Add persons
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body
-
-	if (body.name === undefined) {
-		return response.status(400).json({ error: 'content missing' })
-	}
 
 	const person = new Person({
 		name: body.name,
 		number: body.number,
 	})
 
-	person.save().then(savedPerson => {
-		response.json(savedPerson)
-	})
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // Update person
@@ -90,11 +88,14 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+// Error handling
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
